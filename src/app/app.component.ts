43461770c5658fb5130205151d5777f8c96ff5eb
Lucas Alegre <lucasealegre123@gmail.com>
2023-02-04 14:52:26 -0300

    Edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AngularDonacion';
-  url='https://mercado-pago-alkemi.vercel.app/checkout';
+  url = 'https://mercado-pago-alkemi.vercel.app/checkout';
+
 
   //url ='https://mercado-pago-alkemi-wze7vs3.vercel.app/checkout';
   /*
